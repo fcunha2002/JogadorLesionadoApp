140f950bc88d7d143520e481147a095ce31e563e
fcunha2002 <fcunha2002@gmail.com>
2015-09-27 01:13:49 -0300

    Novas lesões e países.

--- app/src/main/java/com/fabio/jogadorlesionado/negocio/TipoLesao.java
+++ app/src/main/java/com/fabio/jogadorlesionado/negocio/TipoLesao.java
@@ -4,24 +4,25 @@ package com.fabio.jogadorlesionado.negocio;
  * Created by Fabio on 31/08/2015.
  */
 public enum TipoLesao {
-    KNEE_INJURY (1, "knee_injury"),
-    GROIN_STRAIN (2, "groin_strain"),
-    HAMSTRING (3, "hamstring"),
-    CALF_MUSCLE_STRAIN (4, "calf_muscle_strain"),
-    ANKLE_FOOT_INJURY (5, "ankle_foot_injury"),
-    ILLNESS (6, "illness"),
-    THIGH_MUSCLE_STRAIN (7, "thigh_muscle_strain"),
-    ANKLE_LIGAMENTS (8, "ankle_ligaments"),
-    FIBULA_FRACTURE (9, "fibula_fracture"),
-    ACHILLES_TENDONITIS (10, "achilles_tendonitis"),
-    CALF_SHIN_INJURY (11, "calf_shin_injury"),
-    BROKEN_NOSE (12, "broken_nose"),
-    ACL_KNEE_LIGAMENT (13, "acl_knee_ligament"),
-    MCL_KNEE_LIGAMENT (14, "mcl_knee_ligament"),
-    BACK_INJURY (15, "back_injury"),
+    ACHILLES_TENDONITIS (1, "achilles_tendonitis"),
+    ACL_KNEE_LIGAMENT (2, "acl_knee_ligament"),
+    ANKLE_FOOT_INJURY (3, "ankle_foot_injury"),
+    ANKLE_LIGAMENTS (4, "ankle_ligaments"),
+    BACK_INJURY (5, "back_injury"),
+    BROKEN_NOSE (6, "broken_nose"),
+    BROKEN_TOE (7, "broken_toe"),
+    CALF_MUSCLE_STRAIN (8, "calf_muscle_strain"),
+    CALF_SHIN_INJURY (9, "calf_shin_injury"),
+    FIBULA_FRACTURE (10, "fibula_fracture"),
+    GROIN_STRAIN (11, "groin_strain"),
+    HAMSTRING (12, "hamstring"),
+    HIP_THIGH_INJURY (13, "hip_thigh_injury"),
+    ILLNESS (14, "illness"),
+    KNEE_INJURY (15, "knee_injury"),
     KNOCK (16, "knock"),
-    VIRUS (17, "virus"),
-    HIP_THIGH_INJURY (18, "hip_thigh_injury");
+    MCL_KNEE_LIGAMENT (17, "mcl_knee_ligament"),
+    THIGH_MUSCLE_STRAIN (18, "thigh_muscle_strain"),
+    VIRUS (19, "virus");
 
     private long id;
     private String stringVal;
